1f25cdfcfa201d92216f37f1264168601e774d3e
ahmedtamer <alqad.a.119@isg.edu.sa>
2024-01-10 14:41:30 -0600

    Added Crud Operations

--- Server/index.js
+++ Server/index.js
@@ -8,23 +8,87 @@ const Users=require("./models/users.js")
 dotenv.config();
 console.log(process.env.port,process.env.mongourl);
 const connectToDb=async()=>{
-    mongoose.connect(process.env.mongourl)
-    .then(()=>console.log("Success connecting to DB"))
-    .catch((err)=>{
+    try {
+        await mongoose.connect(process.env.mongourl)
+        console.log("Success connecting to DB")
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+}
+const findAll=async()=>{
+    try{
+    const result=await Notes.find({}).exec()
+    return result
+    }
+    catch(err){
         console.log(err)
-    })
+    }}
+
+const insertNote=async(note)=>{
+    try {
+        Notes.insertMany({
+            text:note
+        })
+        .then(res=>console.log(res))
+    } catch (error) {
+        console.log(error)
+    }
+   
+}
+const deleteNote=async(noteID)=>{
+    if (!noteID){
+        console.log("NoteID invalid")
+    }
+    else{
+        try {
+            Notes.deleteOne({_id:noteID})
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
 app.use(cors())
 app.use(express.json())
-app.get("/getAllNotes",(req,res)=>{
+//          ---------------------------------------------------ROUTES------------------------------------------------------------------
+app.get("/getAllNotes",async(req,res)=>{
+            await connectToDb()
+            try {
+                const result=await findAll()
+                console.log(result)
+                res.status(200).send(result)
+            } catch (error) {
+                console.log(error)
+                res.status(400).send("Failed to grab notes")
+            }
+        })
+app.post("/addNote",(req,res)=>{
     try {
         connectToDb
         .then(()=>{
             try {
-                Notes.find({})
-                res.status(200).send("All Notes Fetched")
+                insertNote(`${req.body}`) //double check this
+                res.status(200).send("Note Inserted")
             } catch (error) {
-                res.status(400).send("Failed to grab notes")
+                res.status(400).send("Failed to insert notes")
+            }
+        })
+    } catch (error) {
+        res.status(404).send("Failed")
+    }
+})
+app.get("/get",(req,res)=>{
+    res.send("Hello")
+})
+app.post("/deleteNote",(req,res)=>{
+    try {
+        connectToDb
+        .then(()=>{
+            try {
+                insertNote(`${req.body}`) //double check this
+                res.status(200).send("Note Deleted")
+            } catch (error) {
+                res.status(400).send("Failed to delete notes")
             }
         })
     } catch (error) {
